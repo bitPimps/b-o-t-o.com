7f7414815f0d385e495dd99f8dbe3926f441c391
bitPimps <bitpimps@gmail.com>
2020-02-03 14:37:38 -0600

    Update index.php
    Upgrading jQuery:
    1.11.2 > 11.12.4
    
    Upgrading Bootstrap:
    3.3.7 > 3.4.1

--- wwwRoot/index.php
+++ wwwRoot/index.php
@@ -8,7 +8,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Boto: Barral Osteopathic Teaching Organization</title>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
 		<style>
 			html,body {
 				width: 100%;
